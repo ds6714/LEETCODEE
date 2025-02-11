3edf21df82ab6abb939d827efbfac3419d7bd859
ds6714 <143233586+ds6714@users.noreply.github.com>
2025-02-11 11:47:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Words/reverse-words.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to reverse words in a given string.
+    string reverseWords(string &s) {
+        stringstream ss(s);
+        string ans, word = "";
+        while(ss >> word){
+            ans = word + " " + ans;
+        }
+        if(!ans.empty())
+            ans.pop_back();
+        return ans;
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;     // Read the number of test cases
+    cin.ignore(); // Consume the newline character after reading the integer
+
+    while (t--) {
+        string s;
+        getline(cin, s); // Read the string input
+
+        string str = s.substr(1, s.size() - 2); // Remove surrounding quotes
+        Solution obj;                       // Create an object of the Solution class
+        string ans = obj.reverseWords(str); // Reverse the words in the string
+        cout << '"';                        // Print opening quote
+        cout << ans;                        // Print the result
+        cout << '"';                        // Print closing quote
+        cout << endl;                       // Print a newline
+    }
+    return 0; // Return 0 to indicate successful execution
+}
+
+// } Driver Code Ends
